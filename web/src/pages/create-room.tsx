b6a4d1e982524abd38e565f4985709394cc87a90
César Daniel Rodrigues <cesar.daniel.rodrigues@gmail.com>
2026-02-11 23:52:37 +0000

    1:24:00

--- web/src/pages/create-room.tsx
+++ web/src/pages/create-room.tsx
@@ -1,9 +1,14 @@
-import {Link} from "react-router-dom"
-export function CreateRoom(){
-  return (
-    <div>
-      <div>Create Room</div>
-      <Link to="/room">Acessar sala</Link>
-    </div>
-  )
+import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
+export function CreateRoom() {
+	const {} = useQuery({
+		queryKey: [""],
+	});
+
+	return (
+		<div>
+			<div>Create Room</div>
+			<Link to="/room">Acessar sala</Link>
+		</div>
+	);
 }

ad4f0f5ae08afc12049a2c5e9a2f9d340134a7e0
César Daniel <cesar.daniel.rodrigues@gmail.com>
2026-03-15 10:59:45 -0300

    Finalização da aula

--- web/src/components/create-room-form.tsx
+++ web/src/components/create-room-form.tsx
@@ -1,44 +1,91 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { Card, CardContent, CardDescription, CardHeader } from "./ui/card";
 import z from "zod";
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Form, FormField, FormItem } from "./ui/form";
-
+import { Button } from "./ui/button";
+import { Card, CardContent, CardDescription, CardHeader } from "./ui/card";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
+import { useCreateRoom } from "@/http/use-create-room";
 
 const createRoomSchema = z.object({
-  name: z.string().min(3, { message: 'Inclua no mínimo 3 caracteres' }),
-  description: z.string()
-})
+	name: z.string().min(3, { message: "Inclua no mínimo 3 caracteres" }),
+	description: z.string(),
+});
 
-type CreateRoomFormData = z.infer<typeof createRoomSchema>
+type CreateRoomFormData = z.infer<typeof createRoomSchema>;
 
 export function CreateRoomForm() {
-  const createRoomForm = useForm<CreateRoomFormData>({
-    resolver: zodResolver(createRoomSchema)
-  })
-
-  function handleCreateRoom(data: CreateRoomFormData) {
-    console.log(data)
-  }
-  return (
-    <Card>
-      <CardHeader>Criar sala</CardHeader>
-      <CardDescription>Crie uma nova sala para começar a fazer perguntas e respostas da I. A.</CardDescription>
-      <CardContent>
-        <Form {...createRoomForm}>
-          <form onSubmit={createRoomForm.handleSubmit(handleCreateRoom)} className="flex flex-col gap-4">
-          <FormField control={createRoomForm.control} name="name" render={({field})=>{
-            return(
-              <FormItem>
-
-              </FormItem>
-            )
-          }}/>
+  const {mutateAsync: createRoom} = useCreateRoom()
 
 
-          </form>
-        </Form>
-      </CardContent>
-    </Card>
-  )
+	const createRoomForm = useForm<CreateRoomFormData>({
+		resolver: zodResolver(createRoomSchema),
+		defaultValues: {
+			name: "",
+			description: "",
+		},
+	})
+
+	async function handleCreateRoom({name, description}: CreateRoomFormData) {
+		await createRoom({name, description})
+    createRoomForm.reset()
+	}
+	return (
+		<Card>
+			<CardHeader>Criar sala</CardHeader>
+			<CardDescription>
+				Crie uma nova sala para começar a fazer perguntas e respostas da I. A.
+			</CardDescription>
+			<CardContent>
+				<Form {...createRoomForm}>
+					<form
+						onSubmit={createRoomForm.handleSubmit(handleCreateRoom)}
+						className="flex flex-col gap-4"
+					>
+						<FormField
+							control={createRoomForm.control}
+							name="name"
+							render={({ field }) => {
+								return (
+									<FormItem>
+										<FormLabel>Nome da Sala</FormLabel>
+										<FormControl>
+											<Input {...field} placeholder="Digite o nome da sala" />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								);
+							}}
+						/>
+						<FormField
+							control={createRoomForm.control}
+							name="description"
+							render={({ field }) => {
+								return (
+									<FormItem>
+										<FormLabel>Descrição da sala</FormLabel>
+										<FormControl>
+											<Textarea {...field} />
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								);
+							}}
+						/>
+						<Button type="submit" className="w-full">
+							Criar Sala
+						</Button>
+					</form>
+				</Form>
+			</CardContent>
+		</Card>
+	)
 }
